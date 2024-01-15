8c9391173a1a2e6f5c2d22bc34cdc6eb0997687f
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2024-01-15 12:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+
 function stringChop(str, size) {
   // your code here
+  let s = "";
+  let arr = [];
+	
+  for(let i = 0;i<str.length;i = i +size){
+     arr.push(str.substring(i,i+size));
+  }
+	
+
+  return arr;
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
